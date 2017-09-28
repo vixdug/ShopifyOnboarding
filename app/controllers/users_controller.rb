f32ffc815d2563bba019a6a520490c674bc27efa
Victoria Duggan <victoria.duggan@shopify.com>
2017-09-28 16:04:24 -0400

    add destroy action and link_to

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,12 @@ class UsersController < ApplicationController
     @user= User.find(params[:id])
   end
 
+  def destroy
+  @user = User.find(params[:id])
+  @user.destroy
+end
+
+
   private
 
   def user_params
