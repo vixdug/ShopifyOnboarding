2f5d027d7cafab00fa72495beedec0155f8d4e3d
Victoria Duggan <victoria.duggan@shopify.com>
2017-09-29 17:32:33 -0400

    refactor login and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
 
+  root 'dashboards#hello'
+
   get '/hello' => 'dashboards#hello'
   get '/hi' => 'dashboards#hi'
   get '/personalized/:name' => 'dashboards#personalized'
   get '/reversed/:name' => 'dashboards#reversed'
 
   resources :users
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
 end
